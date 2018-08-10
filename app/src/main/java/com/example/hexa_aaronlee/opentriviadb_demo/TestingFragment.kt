a37687d63af8290326a958b5e553618c1dd9e83c
AaronHexa <jian96@gmail.com>
2018-08-10 17:40:55 +0800

    - Testing to retrieved the data include ( question, question count, category, reset , request token) - Testing on a testing fragment

--- app/src/main/java/com/example/hexa_aaronlee/opentriviadb_demo/TestingFragment.kt
+++ app/src/main/java/com/example/hexa_aaronlee/opentriviadb_demo/TestingFragment.kt
@@ -1,16 +1,13 @@
 package com.example.hexa_aaronlee.opentriviadb_demo
 
-import android.content.Context
-import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.hexa_aaronlee.opentriviadb_demo.API.TokenAPI
-import com.example.hexa_aaronlee.opentriviadb_demo.Object_and_Data.TokenData
-import com.example.hexa_aaronlee.opentriviadb_demo.Object_and_Data.TokenObject
+import com.example.hexa_aaronlee.opentriviadb_demo.API.*
+import com.example.hexa_aaronlee.opentriviadb_demo.ObjectData.*
 import io.reactivex.Observable
 import io.reactivex.Observer
 import io.reactivex.android.schedulers.AndroidSchedulers
@@ -18,21 +15,25 @@ import io.reactivex.disposables.Disposable
 import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
-import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.jackson.JacksonConverterFactory
 import com.fasterxml.jackson.annotation.JsonAutoDetect
 import com.fasterxml.jackson.databind.introspect.VisibilityChecker
 import com.fasterxml.jackson.databind.DeserializationFeature
-import java.lang.Compiler.disable
 import com.fasterxml.jackson.databind.ObjectMapper
-
-
 
 
 class TestingFragment : Fragment() {
 
     lateinit var jsonApi: TokenAPI
-    lateinit var mObservable: Observable<TokenObject>
+    lateinit var resetApi: ResetApi
+    lateinit var mObservable: Observable<TokenData>
+    lateinit var mToken : String
+    var token_response_code : Long = 0
+    lateinit var mapper: ObjectMapper
+    lateinit var retrofit : Retrofit
+    lateinit var mQuestionCountApi: QuestionCountApi
+    lateinit var mCategoryApi: CategoryApi
+    lateinit var mQuestionApi: QuestionApi
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -43,15 +44,28 @@ class TestingFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-       /* val mapper = ObjectMapper()
+        mapper = ObjectMapper()
         mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-        mapper.visibilityChecker = VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY)*/
+        mapper.visibilityChecker = VisibilityChecker.Std.defaultInstance().withFieldVisibility(JsonAutoDetect.Visibility.ANY)
 
-        val retrofit = Retrofit.Builder()
-                .baseUrl("https://opentdb.com")
-                .addConverterFactory(JacksonConverterFactory.create())
+        retrofit = Retrofit.Builder()
+                .baseUrl("https://opentdb.com/")
+                .addConverterFactory(JacksonConverterFactory.create(mapper))
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .build()
+
+        //RequestToken()
+
+        //ResetToken()
+
+        //RequestCategory()
+
+        //RequestAllQuestionCount()
+
+        RequestQuestion()
+    }
+
+    fun RequestToken(){
 
         jsonApi = retrofit.create(TokenAPI::class.java)
 
@@ -59,10 +73,12 @@ class TestingFragment : Fragment() {
 
         mObservable.subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(object : Observer<TokenObject>
+                .subscribe(object : Observer<TokenData>
                 {
-                    override fun onNext(t: TokenObject) {
-                        Log.i("data", t.data!![0].token)
+                    override fun onNext(t: TokenData) {
+                        Log.i("Get Data", "${t.token} / ${t.response_code}")
+                        token_response_code = t.response_code
+                        mToken = t.token
                     }
 
                     override fun onComplete() {
@@ -78,31 +94,131 @@ class TestingFragment : Fragment() {
                     }
 
                 })
+    }
 
+    fun ResetToken(){
+        mToken = "ac7e4f8a320d72f9ed3c9a1b416e54915fce0ea88208aac0e1d35455611bb52f"
+        val resetUrl = "api_token.php?command=reset&token="
+
+        resetApi = retrofit.create(ResetApi::class.java)
+
+        val mObservable = resetApi.resetToken(resetUrl+mToken)
+
+        mObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<ResetData>
+                {
+                    override fun onNext(t: ResetData) {
+                        Log.i("Get Data", "${t.token} / ${t.response_code}")
+                    }
+
+                    override fun onComplete() {
+
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e("Error",e.toString())
+                    }
+
+                })
+    }
+
+    fun RequestCategory(){
+        mCategoryApi = retrofit.create(CategoryApi::class.java)
+
+        val mObservable = mCategoryApi.getAllCategoryData()
+
+        mObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<CategoryData>
+                {
+                    override fun onNext(t: CategoryData) {
+                        Log.i("Get Data", "${t.triviaCategories[0].name} / ${t.triviaCategories[0].id}")
+                    }
+
+                    override fun onComplete() {
+
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e("Error",e.toString())
+                    }
+
+                })
+    }
+
+    fun RequestAllQuestionCount(){
+        mQuestionCountApi = retrofit.create(QuestionCountApi::class.java)
+
+        val mObservable = mQuestionCountApi.getQuestionCountData()
+
+        mObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<QuestionCountData>
+                {
+                    override fun onNext(t: QuestionCountData) {
+                        Log.i("Get Data", "overall : ${t.overall.totalNumOfVerifiedQuestions} / category id 9 : ${t.categories.categoryItem9.totalNumOfVerifiedQuestions}")
+                    }
+
+                    override fun onComplete() {
+
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e("Error",e.toString())
+                    }
+
+                })
     }
 
 
+    fun RequestQuestion(){
+        mToken = "ac7e4f8a320d72f9ed3c9a1b416e54915fce0ea88208aac0e1d35455611bb52f"
+        val questionUrl = "api.php?amount=1&category=30&token="
 
-    /*fun getApiService(enableHTTPLogging: Boolean?): ApiService {
-        if (ApiService == null) {
-            Logger.info("No instance of service yet. Building...")
-            val objectMapper = ObjectMapper().registerModule(Jdk8Module())
-            val retrofitBuilder = Retrofit.Builder()
-                    .baseUrl("https://api.telegram.org/bot$Token/")
-                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                    .addConverterFactory(JacksonConverterFactory.create(objectMapper))
-            if (enableHTTPLogging!!) {
-                Logger.info("Adding request logging...")
-                val httpLoggingInterceptor = HttpLoggingInterceptor()
-                httpLoggingInterceptor.setLevel(HttpLoggingInterceptor.Level.BODY)
-                val httpClient = OkHttpClient.Builder()
-                httpClient.addInterceptor(httpLoggingInterceptor)
-                retrofitBuilder.client(httpClient.build())
-            }
-            val retrofit = retrofitBuilder.build()
-            ApiService = retrofit.create<TelegramBotApiService>(TelegramBotApiService::class.java!!)
-        }
-        Logger.debug("Returning ApiService instance.")
-        return ApiService
-    }*/
+        //&category=__ (selected category id)
+
+        //&difficulty=__ (easy,medium,hard)
+
+        //Type of answer only multiple or boolean
+        //&type=__ (multiple/ boolean)
+
+        mQuestionApi = retrofit.create(QuestionApi::class.java)
+
+        val mObservable = mQuestionApi.getSelectedQuestion(questionUrl+mToken)
+
+        mObservable.subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<QuestionData>
+                {
+                    override fun onNext(t: QuestionData) {
+                        Log.i("Get Data", "Response Code : ${t.responseCode} / Question Category : ${t.results[0].category}")
+                    }
+
+                    override fun onComplete() {
+
+                    }
+
+                    override fun onSubscribe(d: Disposable) {
+
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Log.e("Error",e.toString())
+                    }
+
+                })
+    }
 }
