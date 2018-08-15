62ecb3af044cdd69e36443212d4628cd9e6c2183
AaronHexa <jian96@gmail.com>
2018-08-15 17:38:40 +0800

    - Change some error in Shared preference

--- app/src/main/java/com/example/hexa_aaronlee/opentriviadb_demo/MySharedPreference/SharedPreference.kt
+++ app/src/main/java/com/example/hexa_aaronlee/opentriviadb_demo/MySharedPreference/SharedPreference.kt
@@ -9,9 +9,11 @@ class SharedPreference(val mContext : Context) {
     val PREFERENCE_Category = "Category"
     val PREFERENCE_Difficulty = "Difficulty"
     val PREFERENCE_Type = "Type"
+    val PREFERENCE_Token = "Token"
 
     val preference = mContext.getSharedPreferences(PREFERENCE_NAME,Context.MODE_PRIVATE)
 
+    //... Selection about Category
     fun getCategory() : String{
         return preference.getString(PREFERENCE_Category,"")
     }
@@ -22,23 +24,36 @@ class SharedPreference(val mContext : Context) {
         editor.apply()
     }
 
+    //... Selection about difficulty
     fun getDifficulty() : String{
-        return preference.getString(PREFERENCE_Category,"")
+        return preference.getString(PREFERENCE_Difficulty,"")
     }
 
-    fun setDifficulty(category: String){
+    fun setDifficulty(difficulty: String){
         val editor = preference.edit()
-        editor.putString(PREFERENCE_Category,category)
+        editor.putString(PREFERENCE_Difficulty,difficulty)
         editor.apply()
     }
 
+    //... Selection about Type of Question
     fun getType() : String{
-        return preference.getString(PREFERENCE_Category,"")
+        return preference.getString(PREFERENCE_Type,"")
     }
 
-    fun setType(category: String){
+    fun setType(type: String){
         val editor = preference.edit()
-        editor.putString(PREFERENCE_Category,category)
+        editor.putString(PREFERENCE_Type,type)
+        editor.apply()
+    }
+
+    //... Token Api
+    fun getToken() : String{
+        return preference.getString(PREFERENCE_Token,"")
+    }
+
+    fun setToken(token: String){
+        val editor = preference.edit()
+        editor.putString(PREFERENCE_Type,token)
         editor.apply()
     }
 }
